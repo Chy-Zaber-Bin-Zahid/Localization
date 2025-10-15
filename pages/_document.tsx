d078df807a65543bfad272ec291631fa1a4cb942
Chy-Zaber-Bin-Zahid <zaber.zahid@monstar-lab.com>
2025-10-15 22:42:49 +0600

    fix: fix type error on document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
 
-export default function Document(props: any) {
+export default function Document(props: DocumentProps) {
   return (
     <Html lang={props.locale}>
       <Head />
